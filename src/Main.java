20f83d41ae504c94cc48f00ceac75505a57fec62
guiPython <guipython2001@gmail.com>
2023-09-05 19:24:30 -0300

    fix: close scanner resource

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu(new Scanner(System.in));
+        Scanner scanner = new Scanner(System.in);
+
+        Menu menu = new Menu(scanner);
         Phone phone = new PhoneApp();
         MusicPlayer musicPlayer = new IPod();
         InternetBrowser internetBrowser = new Safari();
 
         Iphone iphone = new Iphone(phone, musicPlayer, internetBrowser);
         menu.execute(iphone);
+
+        scanner.close();
     }
 }
