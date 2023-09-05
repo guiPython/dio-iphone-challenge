8a5b48083645f5b9ad1c4a921a2931e4878ff926
guiPython <guipython2001@gmail.com>
2023-09-05 15:59:14 -0300

    feat: add iphone class

--- src/Iphone.java
+++ src/Iphone.java
@@ -1,2 +1,24 @@
+
 public class Iphone {
+    public final Phone phone;
+    public final MusicPlayer musicPlayer;
+    public final InternetBrowser internetBrowser;
+    private boolean on;
+
+    public Iphone(Phone phone, MusicPlayer musicPlayer, InternetBrowser internetBrowser) {
+        this.phone = phone;
+        this.musicPlayer = musicPlayer;
+        this.internetBrowser = internetBrowser;
+        this.on = true;
+        System.out.println("\nIphone ligado");
+    }
+
+    public boolean isOn() {
+        return this.on;
+    }
+
+    public void turnOff() {
+        this.on = false;
+        System.out.println("\nIphone desligado.");
+    }
 }
