a3fd90675146efc17f040c6e1c744f8793adc8c1
guiPython <guipython2001@gmail.com>
2023-09-05 16:00:27 -0300

    feat: add and config entry point

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Menu menu = new Menu(new Scanner(System.in));
+        Phone phone = new PhoneApp();
+        MusicPlayer musicPlayer = new IPod();
+        InternetBrowser internetBrowser = new Safari();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Iphone iphone = new Iphone(phone, musicPlayer, internetBrowser);
+        menu.execute(iphone);
     }
 }
