aff50ed96cc55b7621c2cc04728711f70a3453e0
guiPython <guipython2001@gmail.com>
2023-09-05 15:58:41 -0300

    feat: add internet browser interface and safari implementation

--- src/Safari.java
+++ src/Safari.java
@@ -1,2 +1,16 @@
-public class Safari {
+public class Safari implements InternetBrowser {
+    @Override
+    public void show() {
+        System.out.println("\nApresentando pagina no Safari.\n");
+    }
+
+    @Override
+    public void newTab() {
+        System.out.println("\nAbrindo uma nova aba no Safari.\n");
+    }
+
+    @Override
+    public void refresh() {
+        System.out.println("\nRecarregando a pagina no Safari.\n");
+    }
 }
